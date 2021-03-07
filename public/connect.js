1737b0cee1e9a433f6aab0ee2f7035e6427f8ffa
Joseph Ebin <josefophe@gmail.com>
2021-03-07 13:04:57 +0100

    final edit

---
+++ public/connect.js
@@ -0,0 +1,5 @@
+function hello ()
+{
+	alert('Welcome to LEADHERSHIP! You are connected to your mentor please click ok to connect with them through slack');
+	window.location.href = "https://app.slack.com/client/T01QTGTDGPK/C01QTGTSG0H";
+}
